54591404c3aa1cc2e591dbdc3abdb9092380e59c
Kevin Xu <kvnlxu@gmail.com>
2017-05-14 01:20:06 -0700

    Testing ObservatioFromEntity currently diabled normal policies agent summons cow and diamond sword to environment as part of test

--- source/maze.py
+++ source/maze.py
@@ -22,11 +22,18 @@ while True:
 
 		#agent performs movement action here
 		observations = m.get_observation()
-		if action == '' or observations.at_junction():
+
+		########testing observations with a cow and diamond sword
+		m.agent_host.sendCommand('chat /summon Item 6 56 0 {Item:{id:minecraft:diamond_sword,Count:1}}')
+		m.agent_host.sendCommand('chat /summon Cow 5 56 0')
+		#########################################################
+
+		
+		'''if action == '' or observations.at_junction():
 			action = h.random_direction(observations)
 			print
 			print action
-		m.send_command(action)
+		m.send_command(action)'''
 		m.check_errors()
 		
 		time.sleep(0.3)
