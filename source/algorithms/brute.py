67dcc8d9b66ca23368ab1a6bab14903fb7b6db9d
Kevin Xu <kvnlxu@gmail.com>
2017-05-22 21:48:07 -0700

    Brute force algorithm added implementation of brute force algorithm it explores all possible combinations of policy strings until performance stops increasing

---
+++ source/algorithms/brute.py
@@ -0,0 +1,46 @@
+import random
+
+from algorithm import algorithm
+
+class brute(algorithm):
+	def __init__(self, set_actions, init_str = []):
+		super(greedy, self).__init__(set_actions)
+		self.sLen = 1
+		self.local_space = self.generate_all_strings()
+		self.space_scores = []
+		self.space_index = 0
+		self.move = 0
+		self.max = 0
+
+	def generate_next_strings(self):
+		all_strings = self.space_scores
+		if len(all_strings) == 0:
+			for i in range(self.possible_actions):
+				all_strings.append([self.possible_actions(i)])
+		else:
+			for j in range(len(all_strings)):
+				for k in range(len(self.possible_actions)):
+					all_strings.append(all_strings[j] + [self.possible_actions(k)])
+
+		return all_strings
+			
+
+	def set_score(self, score):
+		print ("String: " + str([h.__name__[0] for h in self.local_space[self.space_index]]))
+
+		self.space_scores.append(score)
+		self.space_index += 1
+		self.move = 0
+
+		if self.space_index >= len(self.local_space):
+                        newmax = max(space_scores)
+                        if(newmax > self.max):
+                                self.max = newmax
+                                self.generate_next_strings()
+
+	def get_action(self, obs):
+		curr_str = self.local_space[self.space_index]
+		a = curr_str[self.move % len(curr_str)]
+		self.move += 1
+		return a(obs)
+		
