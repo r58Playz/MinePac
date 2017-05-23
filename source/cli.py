3c74547b7b5505a5de7012067b30626d35fd24a1
Kevin Xu <kvnlxu@gmail.com>
2017-05-22 21:46:52 -0700

    Added new commands quick hill climbing/annealing added brute force method added

--- source/cli.py
+++ source/cli.py
@@ -4,16 +4,24 @@ import os
 
 from algorithms.genetic import genetic
 from algorithms.hillclimbing import climber
+from algorithms.hillclimbing2 import climber2
+from algorithms.brute import brute
 
 import heuristics as h
 
 def get_algorithm(alg_selected):
     if alg_selected == "genetic":
         return genetic([h.towards_item, h.away_from_enemy, h.random_direction])
-    elif alg_selected == "hillclimb" or alg_selected == "hilclimbing":
+    elif alg_selected == "hillclimb" or alg_selected == "hillclimbing":
         return climber([h.towards_item, h.away_from_enemy])#, h.random_direction])
     elif alg_selected == "annealing" or alg_selected == "anneal":
         return climber([h.towards_item, h.away_from_enemy], 1.0, 0.5)#, h.random_direction])
+    elif alg_selected == "hillclimb2" or alg_selected == "hillclimbing2":
+        return climber2([h.towards_item, h.away_from_enemy])
+    elif alg_selected == "annealing2" or alg_selected == "anneal2":
+        return climber2([h.towards_item, h.away_from_enemy], 1.0, 0.5)
+    elif alg_selected == "brute":
+        return brute([h.towards_item, h.away_from_enemy, h.random_direction])
     elif alg_selected == "mcts":
         pass
     else: pass
@@ -23,8 +31,11 @@ def get_algorithm(alg_selected):
 def valid_algorithms(): # dictionary in format 'algorithm name': 'algorithm description'
     return {
                 "genetic": "genetic algorithm operating on strings of movement heuristics",
-                "hillclimb": "hillclimbing algorithm operating on weighted probabilities of using a heuristic",
-                "annealing": "same as the hillclimbing algorithm, but with simulated annealing"
+                "hillclimb": "hillclimbing algorithm operating on strings of movement heuristics",
+                "annealing": "same as the hillclimbing algorithm, but with simulated annealing",
+                "hillclimb2": "hillclimbing that explores fewer neighboring states. Each iteration should be much quicker",
+                "annealing2": "simulated annealing using the hillclimb2 modifications",
+                "brute": "brute force method of finding optimal solution by exploring all possible string combinations"
             }
 
 def algorithms_list():
